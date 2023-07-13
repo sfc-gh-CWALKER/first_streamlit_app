7fb8d746ef05b9c3c14618325d4a0bdb8c8d2147
Carson Walker <carson.walker@snowflake.com>
2023-07-13 10:11:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
 
 
 # dont run anything past here while we troubleshoot
-# streamlit.stop()
+streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
