42bb515ca385bf6d3e91c1e33db7323abe9c41e2
Carson Walker <carson.walker@snowflake.com>
2023-07-13 00:38:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
 # create the repeatable code clock (called function)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
@@ -36,10 +37,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
-     streamlit.error("Please select a fruit to get information.")
+        streamlit.error("Please select a fruit to get information.")
    else:
-     back_from_function = get_fruityvice_data(fruit_choice)
-     streamlit.dataframe(back_from_function)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 
 
 
